033f164fbee4cd5d647ca673022677e1e8d62516
Gustavo Valério <gustavo.v7@gmail.com>
2021-08-27 19:02:22 -0300

    Atualização de execução.
    Verifica se o script foi executado como root, caso contrário, emite uma mensagem. Se executado como sudo, reexecuta o script para corrigir o timeout do sudo que pode, em conexões lentas, afetar a execução do script caso ela exceda o tempo limite do sudo. Função "install" adicionada. Facilita a execução do script em outras distros que possuam os mesmos pacotes nomeados, já que, nesse caso, seria alterado o gerenciador de pacotes apenas na função.

--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+function install(){
+    apt install -y    
+}
+
 function sources_list(){
     ### Habilita pacotes não-livre e de contribuintes
     printf "\nEtapa 1/10: Adicionando contrib non-free na sources.list... \n\n"
-    sudo sed -i 's/main/main contrib non-free/' /etc/apt/sources.list
+    sed -i 's/main/main contrib non-free/' /etc/apt/sources.list
     cat /etc/apt/sources.list
 
 }
@@ -11,7 +15,7 @@ function sources_list(){
 function multi_arch(){
     ### Habilita suporte multi-arquitetura
     printf "\nEtapa 2/10: Habilitando arquitetura i386... \n"
-    sudo dpkg --add-architecture i386
+    dpkg --add-architecture i386
     printf "\nFeito!\n"
 
 }
@@ -19,52 +23,53 @@ function multi_arch(){
 function update_system(){
     ### Atualiza repositórios e pacotes
     printf "\nEtapa 3/10: Atualizando o sistema... \n\n"
-    sudo apt update && sudo apt upgrade
+    apt update && apt upgrade
 
 }
 
 function kde_minimal(){
     ### Instala o ambiente mínimo do KDE Plasma
     printf "\nEtapa 4/10: Instalando o KDE Plasma mínimo... \n"
-    sudo apt install kde-plasma-desktop
+    install kde-plasma-desktop
 
 }
 
 function nvidia_driver(){
     ### Driver proprietário NVIDIA
     printf "\nEtapa 5/10: Instalando driver proprietário NVIDIA... \n\n"
-    sudo apt install nvidia-driver
+    install nvidia-driver
 
 }
 
 function apps(){
     ### Minha seleção de pacotes
     printf "\nEtapa 6/10: Instalando softwares diversos... \n\n"
-    sudo apt install ark bash-completion f{eh,irefox-esr,irefox-esr-l10n-pt-br} i{nkscape,nxi} k{ate,calc,colorchooser,denlive,df,get,rita} l{ibreoffice,ibreoffice-l10n-pt-br,ibreoffice-qt5,ibreoffice-kde5} neofetch p{artitionmanager,lasma-browser-integration,7zip-full,7zip-rar} qbittorrent ristretto s{weeper,ynaptic} telegram-desktop vlc vokoscreen-ng wget youtube-dl
+    install ark bash-completion f{eh,irefox-esr,irefox-esr-l10n-pt-br} i{nkscape,nxi} k{ate,calc,colorchooser,denlive,df,get,rita} l{ibreoffice,ibreoffice-l10n-pt-br,ibreoffice-qt5,ibreoffice-kde5} neofetch p{artitionmanager,lasma-browser-integration,7zip-full,7zip-rar} qbittorrent ristretto s{weeper,ynaptic} telegram-desktop vlc vokoscreen-ng wget youtube-dl
 
 }
 
 function games(){
     ### Pacotes referentes a jogos
     printf "\nEtapa 7/10: Instalando pacotes de jogos... \n\n"
-    sudo apt install lutris steam obs-studio jstest-gtk
+    install lutris steam obs-studio jstest-gtk
 
 }
 
 function emulators(){
     ### Pacotes de emuladores
     printf "\nEtapa 8/10: Instalando emuladores... \n"
-    sudo apt install pcsx2 retroarch
+    install pcsx2 retroarch
 
 }
 
 function discord(){
     ### Baixa e instala o pacote Discord
     printf "\nEtapa 9/10: Instalando o Discord... \n"
-    sudo apt install wget
+    install wget
     wget -cO discord.tar.gz "https://discord.com/api/download?platform=linux&format=tar.gz"
-    sudo tar -xvzf discord.tar.gz -C /opt
-    sudo ln -sf /opt/Discord/Discord /usr/bin/Discord
+    tar -xvzf discord.tar.gz -C /opt
+    ln -sf /opt/Discord/Discord /usr/bin/Discord
+ 
     printf "
 [Desktop Entry]
 Name=Discord
@@ -76,7 +81,7 @@ Icon=/opt/Discord/discord.png
 Type=Application
 Categories=Network;InstantMessaging;
 Path=/usr/bin
-    " | sudo tee /usr/share/applications/discord.desktop
+    " > /usr/share/applications/discord.desktop
     printf "\nDiscord instalado com sucesso!\n"
 
 }
@@ -84,7 +89,7 @@ Path=/usr/bin
 function flatpak(){
     ### Instala o Flatpak e adiciona o repositório Flathub
     printf "\nEtapa 10/10: Adicionando Repositório Flathub... \n\n"
-    sudo apt install flatpak
+    install flatpak
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     printf "\nFeito!\n\n"
 
@@ -93,21 +98,34 @@ function flatpak(){
 function mscorefonts(){
     ### Fontes TrueType da Microsoft
     printf "\nEtapa Opcional: Instalando Fontes TrueType Microsoft... \n\n"
-    apt install ttf-mscorefonts-installer
+    install ttf-mscorefonts-installer
 
 }
 
-sources_list
-multi_arch
-update_system
-kde_minimal
-nvidia_driver
-apps
-games
-emulators
-discord
-flatpak
-#mscorefonts
+
+if [[ "$1" == "exec" ]] && [[ $UID -eq 0 ]]; then
+    sources_list
+    multi_arch
+    update_system
+    kde_minimal
+    nvidia_driver
+    apps
+    games
+    emulators
+    discord
+    flatpak
+    #mscorefonts
+elif [[ $UID -eq 0 ]]; then
+    # Corrige o timeout do sudo.
+    # Se a conexão do usuário for lenta,
+    # o script demorará mais que o tempo permitido
+    # para a execução do SUDO. Para evitar o timeout,
+    # chamamos o script novamente mudando os poderes do sudo.
+    sudo su -c "$0 exec"
+else
+    printf 'Precisa ser root!\n'
+    exit 255
+fi
 
 ### Fim do script.
 printf "\nFIM DO SCRIPT \n\n"
